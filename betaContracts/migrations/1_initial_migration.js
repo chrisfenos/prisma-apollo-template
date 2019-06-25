709a6470bb3d53d04f5889e75b9b9534b224d82a
Chris Fenos <chris.m.fenos@gmail.com>
2019-06-24 22:40:29 -0400

    remove contracts from root

--- betaContracts/migrations/1_initial_migration.js
+++
@@ -1,9 +0,0 @@
-const Migrations = artifacts.require("Migrations");
-const DomainProxy = artifacts.require("DomainProxy");
-const Url = artifacts.require("Url");
-
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
-  deployer.deploy(DomainProxy);
-  deployer.deploy(Url);
-};
